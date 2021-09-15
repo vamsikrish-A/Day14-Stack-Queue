16df5a6a25b5459cf828469c821f20bebc756a72
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-15 16:18:20 +0530

    comments

--- src/com/bridgelabz/StackQueue.java
+++ src/com/bridgelabz/StackQueue.java
@@ -1,6 +1,13 @@
 package com.bridgelabz;
 
 public class StackQueue {
+    /*
+    @author: Vamsi Krishna A
+
+    @purpose: Stack and Queues
+
+    @date: 13/09/2019
+     */
     public static void main(String[] args) {
         System.out.println("Welcome!! ");
     }
