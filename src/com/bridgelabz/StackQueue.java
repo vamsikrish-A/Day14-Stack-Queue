c517677b818ba96a1e90366b103151294e4f2eb2
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-15 16:15:44 +0530

    stack elements pushed

--- src/com/bridgelabz/StackQueue.java
+++ src/com/bridgelabz/StackQueue.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Stack;
+
 public class StackQueue {
     public static void main(String[] args) {
         System.out.println("Welcome!! ");
+        //creating Instance of Stack
+        Stack<Integer> st = new Stack<>();
+
+        //pushing element into stack
+        st.push(70);
+        st.push(30);
+        st.push(56);
+
+        //print element to stack
+        System.out.println("Stack Elements: "+st);
+
+
     }
 }
